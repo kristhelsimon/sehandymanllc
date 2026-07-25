8dfe55033a1af316127de969568eb095178b6c75
kristhel-cyber <kristhelsimon08@gmail.com>
2026-07-25 14:45:33 +0800

    Use SVG for Jaime listening image

--- app/page.js
+++ app/page.js
@@ -490,7 +490,7 @@ export default function Home() {
 
         <section className="project-story" id="work">
           <div className="project-image">
-            <img src={assetPath("/jaime-handyman-listening.webp")} alt="Jaime listening to a homeowner discuss her project" />
+            <img src={assetPath("/jaime-handyman-listening.svg")} alt="Jaime listening to a homeowner discuss her project" />
             <span>Craftsmanship you can see</span>
           </div>
           <div className="project-copy">
